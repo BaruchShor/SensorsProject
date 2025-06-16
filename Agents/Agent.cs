5d170c5cc5a0c875f4582bda632b7bbc9118b0ed
Hershi Shor <e0548597918@gmail.com>
2025-06-16 10:08:40 +0300

    new folders and basic classes

---
+++ Agents/Agent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsProject
+{
+    internal class Agent : IAgent
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ IAgent/IAgent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorsProject
+{
+    internal class IAgent
+    {
+        string Name { get; set; }
+        string Type { get; set; }
+        Dictionary<Sensor> SensorsDict { get; set; }
+
+        void Activate() { }
+    }
+}
